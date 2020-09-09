b223f8c824aa1ace473791dca693fbd525b55988
LukeZvada <zvadamusic@gmail.com>
2020-09-08 21:05:08 -0700

    image uploader working

--- src/TourVana.js
+++ src/TourVana.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { NavBar } from "./components/nav/NavBar"
+import { Cloudinary } from "./components/shows/ShowsProvider"
+
+
+
 // import { ApplicationViews } from "./ApplicationViews"
 
 export const TourVana = () => (
+    
+
     <>
         <NavBar />
+        <Cloudinary />
     </>
 )
