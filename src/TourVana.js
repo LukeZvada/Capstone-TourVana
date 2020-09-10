00de96cddd5de81935fa0ed6afe42f3c5c5ba813
LukeZvada <zvadamusic@gmail.com>
2020-09-10 09:36:28 -0700

    added images and database.json

--- src/TourVana.js
+++ src/TourVana.js
@@ -11,6 +11,6 @@ export const TourVana = () => (
 
     <>
         <NavBar />
-        <Cloudinary />
+        {/* <Cloudinary /> */}
     </>
 )
--- src/components/shows/ShowsProvider.js
+++ src/components/shows/ShowsProvider.js
@@ -2,43 +2,66 @@ import React, { useState } from 'react'
 import CachedIcon from '@material-ui/icons/Cached';
 
 
-export const Cloudinary = () => {
-    const [image, setImage] = useState('')
-    const [loading, setLoading] = useState(false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const Cloudinary = () => {
+//     const [image, setImage] = useState('')
+//     const [loading, setLoading] = useState(false)
   
-    const uploadImage = async e => {
-      const files = e.target.files
-      const data = new FormData()
-      data.append('file', files[0])
-      data.append('upload_preset', 'lzvada')
-      setLoading(true)
-      const res = await fetch(
-        '	https://api.cloudinary.com/v1_1/zvada/image/upload',
-        {
-          method: 'POST',
-          body: data
-        }
-      )
-      const file = await res.json()
-  
-      setImage(file.secure_url)
-      setLoading(false)
-    }
-  
-    return (
-      <div className="Upload">
-        <input
-          type="file"
-          name="file"
-          placeholder="Upload an image"
-          onChange={uploadImage}
-        />
-        {loading ? (
-          <h3><CachedIcon /></h3>
-        ) : (
-          <img src={image} style={{ width: '300px' }} />
-        )}
-      </div>
-    )
-  }
+//     const uploadImage = async e => {
+//         const files = e.target.files
+//         const data = new FormData()
+//         data.append('file', files[0])
+//         data.append('upload_preset', 'lzvada')
+//         setLoading(true)
+//         const res = await fetch(
+//             '	https://api.cloudinary.com/v1_1/zvada/image/upload',
+//             {
+//             method: 'POST',
+//             body: data
+//             }
+//         )
+//         const file = await res.json()
+    
+//         setImage(file.secure_url)
+//         setLoading(false)
+
+//         // here, I will use the file.secure_url and save that url to your database.json
+//         // you'll create something like a settlementProvider file that includes methods to get/post/etc to your database (like EmployeeProvider does in your other project)
+//         // you'll import that file above
+//         // then here, you'll call the add method...probably addSettlement() and pass in the image url and the show id (still need to do the work to have the show id)
+//     }
+
+//     return (
+//       <div className="Upload">
+//         <input
+//           type="file"
+//           name="file"
+//           placeholder="Upload an image"
+//           onChange={uploadImage}
+//         />
+//         {loading ? (
+//           <h3><CachedIcon /></h3>
+//         ) : (
+//           <img src={image} style={{ width: '300px' }} />
+//         )}
+//       </div>
+//     )
+//   }
   
