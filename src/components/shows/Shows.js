f25e7ddb3cb14ebf0e734f0177f067d1e1079cf1
LukeZvada <zvadamusic@gmail.com>
2020-09-10 15:17:46 -0700

    added proper padding to nav bar and center links. Added project name and logo to public folder

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -1,12 +1,12 @@
-import React from "react"
-import "./Shows.css"
+// import React from "react"
+// import "./Shows.css"
 
 
-export const Show = ({ show }) => (
-    <section key={show.id} className="show">
-        <div><h3>{show.timestamp}</h3></div>
-        <div>{show.venueName}</div>
-        <div>Owner: {show.city}</div>
-        <div>Location: {show.state}</div>
-    </section>
-)
+// export const Show = ({ show }) => (
+//     <section key={show.id} className="show">
+//         <div><h3>{show.timestamp}</h3></div>
+//         <div>{show.venueName}</div>
+//         <div>Owner: {show.city}</div>
+//         <div>Location: {show.state}</div>
+//     </section>
+// )
